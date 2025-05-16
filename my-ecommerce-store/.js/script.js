84168fe0c0fad4a86b252f52b7dba77628079522
Desani23 <ilupejud@yahoo.com>
2025-05-16 13:59:35 +0100

    working project

---
+++ my-ecommerce-store/.js/script.js
@@ -0,0 +1,23 @@
+const products = [
+    {
+        id: 1,
+        name: "Product Name 1",
+        price: 19.99,
+        image: "images/product1.jpg",
+        largeImage: "images/product1-large.jpg",
+        description: "This is a detailed description of Product 1...",
+        category: "Electronics",
+        sku: "P12345"
+    },
+    {
+        id: 2,
+        name: "Product Name 2",
+        price: 29.99,
+        image: "images/product2.jpg",
+        largeImage: "images/product2-large.jpg",
+        description: "Details about Product 2...",
+        category: "Apparel",
+        sku: "P67890"
+    },
+    // Add more products
+];
